5bdf4c50f5969ca5ef999dd4200cde58799108e3
BoldyrevaMarya <matazzz@mail.ru>
2024-08-17 18:23:11 +0300

    Update module_5_2.py

--- module_5_2.py
+++ module_5_2.py
@@ -17,10 +17,8 @@ class House:
     def __str__(self):
         return (f'Название "{self.name}", кол-во этажей: "{self.number_of_floors}"')
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
 
 print(h1)
 print(h2)
